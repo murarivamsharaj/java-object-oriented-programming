468d2209e86434aa5c03de5ce70c5063c291b00a
MURARI VAMSHARAJ <155216940+murarivamsharaj@users.noreply.github.com>
2026-04-22 15:36:04 +0530

    feat: new changes added

--- src/com/java/oop/ArrayCodingQuetions.java
+++ src/com/java/oop/ArrayCodingQuetions.java
@@ -4,24 +4,33 @@ import java.util.Arrays;
 
 public class ArrayCodingQuetions {
 
+
     public static int findingHighestPrice(int[] productPrices) {
-
-        int max = productPrices[0];
-
-
+        int highestPrice = productPrices[0];
         for (int i = 1; i < productPrices.length; i++) {
-
-            if (productPrices[i] > max) {
-                max = productPrices[i];
+            if (productPrices[i] > highestPrice) {
+                highestPrice = productPrices[i];
             }
         }
-
-
-        return max;
+        return highestPrice;
     }
 
-    public static void main(String[] args) {
-        int[] productPrices = {10, 20, 30, 40, 50};
-        System.out.println("The highest price is: " + findingHighestPrice(productPrices));
+    public static void printAscending(int[] productPrices) {
+        Arrays.sort(productPrices);
+
+        System.out.print("Prices in Ascending Order: ");
+        for (int price : productPrices) System.out.print(price + " ");
+        System.out.println();
+    }
+
+    static void main(String[] args) {
+
+        int[] productPrices = {50, 10, 40, 20, 30};
+
+
+        int highest = findingHighestPrice(productPrices);
+        System.out.println("Highest Price: " + highest);
+
+        printAscending(productPrices);
     }
 }
