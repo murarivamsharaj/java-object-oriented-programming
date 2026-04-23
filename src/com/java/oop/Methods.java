d89db02bc118dc2791246b45603cc3cd59e5f59b
MURARI VAMSHARAJ <155216940+murarivamsharaj@users.noreply.github.com>
2026-04-23 22:07:49 +0530

    feat: new  changes

--- src/com/java/oop/Methods.java
+++ src/com/java/oop/Methods.java
@@ -14,6 +14,11 @@ public class Methods {
         methods.displayProductDetails();
         int i = methods.multiplyNumbers(6,8);
         System.out.println("print i:" + i);
+        methods.addNubers(45,55);
+        methods.addNubers(103,97);
+        methods.addNubers(145,155);
+        methods.addNubers(200,200);
+        methods.addNubers(250,250);
 
     }
     void displayProductDetails() {
@@ -24,6 +29,10 @@ public class Methods {
     int multiplyNumbers(int a,int b) {
         int x = a * b;
       return x;
+    }
+    void addNubers(int a,int b){
+        System.out.println(a+b);
+
     }
 
 
