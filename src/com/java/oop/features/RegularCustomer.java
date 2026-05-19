6d102220fa376ab2798623f5812c2483d148c3ff
“murarivamsharaj” <chmurari73@gmail.com>
2026-05-19 13:07:37 +0530

    feat: oops features

--- src/com/java/oop/features/RegularCustomer.java
+++ src/com/java/oop/features/RegularCustomer.java
@@ -3,13 +3,19 @@ package com.java.oop.features;
 public class RegularCustomer extends Customer {
     int cashBackPoints;
 
+    public RegularCustomer(int id, String name, String email, long contactNo, byte age, int salary, int cashBackPoints) {
+        super(id, name, email, contactNo, age, salary);
+        this.cashBackPoints = cashBackPoints;
+    }
+
     public RegularCustomer() {
         super();
+        System.out.println("Regular customer called:");
     }
 
     void displayCustomerDetails() {
         super.displayCustomerDetails();
-        System.out.println("show cash                                                                                                                                                                                                                                                                                                                                                                                                                           back points :" + cashBackPoints);
+        System.out.println("cash Back points: " + cashBackPoints);
     }
 }
 
